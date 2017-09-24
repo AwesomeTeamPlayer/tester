60035e7e1af877d1c86e658d35e76f1302ab8931
SimonIsMe <szym1991@gmail.com>
2017-09-24 22:47:50 +0200

    Make test smaller

--- run.js
+++ run.js
@@ -9,18 +9,14 @@ const wsUrl = process.env.WS_URL;
 
 const maxTimeSeconds = process.env.MAX_TIME_SECONDS;
 
-tape.test('Welcome message', (assert) => {
+tape.test('Welcome message', async function (assert) {
 
-    async function runTest()
-    {
-        await client.connect(wsUrl, maxTimeSeconds);
+    await client.connect(wsUrl, maxTimeSeconds);
 
-        let receivedMessage = await client.receiveMessage();
-        assert.equal('Hello world!', receivedMessage);
+    let receivedMessage = await client.receiveMessage();
+    assert.equal('Hello world!', receivedMessage);
 
-        client.close();
-    }
-    runTest();
+    client.close();
 
     assert.end();
 });
